04db0740f394dbf598c4c5dbb1fa8a9f55a027d2
QWERTY-69X <s6503014621191@email.kmutnb.ac.th>
2025-10-02 13:21:03 +0700

    First commit: Blink LED project

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+#define LED_PIN 13
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(LED_PIN, HIGH); // เปิดไฟ
+  delay(500);                  // รอ 0.5 วินาที
+  digitalWrite(LED_PIN, LOW);  // ปิดไฟ
+  delay(500);
+}
